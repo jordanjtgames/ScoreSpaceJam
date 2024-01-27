27a1cc5eb70ab0560ad670b5c6baa9e39e802d20
ManasDey <deyvloper1003@gmail.com>
2024-01-27 20:09:16 +0530

    shapeChange

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -7,13 +7,15 @@ public class MovementScript : MonoBehaviour
 {
     [SerializeField]private float HorSpeed;
     [SerializeField] private float forwardSpeed;
-
+    [SerializeField] private int ShapeIndex = 0;
+    [SerializeField] GameObject[] Shapes;
     [SerializeField] float SpeedMul;
+    [SerializeField] KeyCode ChangeKey;
 
     // Update is called once per frame
     void Update()
     {
-       
+        ShapeSet();
         Move();
 
     }
@@ -28,6 +30,38 @@ public class MovementScript : MonoBehaviour
     {
        forwardSpeed += forwardSpeed * Time.deltaTime * SpeedMul;
         forwardSpeed = Mathf.Clamp(forwardSpeed, 10f, 60f);
+    }
+   void  ShapeSet()
+    {
+
+        if (Input.GetKeyDown(ChangeKey))
+        {
+            ShapeIndex++;
+
+        }
+        if (ShapeIndex > 2)
+        {
+            ShapeIndex = 0;
+
+        }
+        if (ShapeIndex == 0)
+        {
+            Shapes[0].SetActive(true);
+            Shapes[1].SetActive(false);
+            Shapes[2].SetActive(false);
+        }
+        if (ShapeIndex == 1)
+        {
+            Shapes[0].SetActive(false);
+            Shapes[1].SetActive(true);
+            Shapes[2].SetActive(false);
+        }
+        if (ShapeIndex == 2)
+        {
+            Shapes[0].SetActive(false);
+            Shapes[1].SetActive(false);
+            Shapes[2].SetActive(true);
+        }
     }
 
 }
