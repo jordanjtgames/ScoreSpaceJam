6d0468582341cc8e5b1edd6be863b4ef80c21058
ManasDey <deyvloper1003@gmail.com>
2024-01-29 07:25:50 +0530

    MenuUI

---
+++ Assets/startGame.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startGame : MonoBehaviour
+{
+    public void gameStart() 
+    {
+        SceneManager.LoadScene("ProgrammerScene");
+    }
+    public void gameExit() 
+    {
+        Application.Quit();
+    }
+}
